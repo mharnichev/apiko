4a625a436e42c5d4acd5d24c755659cb4753cf11
Mark Harnichev <75042975+mharnichev@users.noreply.github.com>
2021-01-19 00:07:59 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <portal-target name="modal" />
+    <component :is="layout" id="root-wrapper">
+      <navbar></navbar>
+      <router-view />
+    </component>
+  </div>
+</template>
+
+
+<script>
+import DefaultLayout from '@/layouts/default.vue';
+import navbar from "./views/auth/Navbar.vue";
+
+
+export default {
+  computed: {
+    layout() {
+      return (this.$route.meta.layout || 'default-layout');
+    }
+  },
+  components: {
+    DefaultLayout,
+    navbar
+  }
+}
+</script>
+
+<style scoped lang="scss">
+#app {
+  min-height: 100vh;
+  flex: 1 1 auto;
+  display: flex;
+  flex-direction: column;
+}
+</style>
