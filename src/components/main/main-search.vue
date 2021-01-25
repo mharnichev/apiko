ce9800db6ea24280336a702a0928df7a06cd4874
kharnichev <k.harnichev@keepsolid.com>
2021-01-25 22:51:25 +0200

    [upd-project] add new custom selector

---
+++ src/components/main/main-search.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="ms">
+    <custom-wrapper :size="'large'" class="ms__container custom-container">
+      <div class="ms__categories">
+        <custom-selector
+            :options="['cheap', 'hot offer', 'expensive']"
+            :default="'go'"
+            class="select"
+            @input="alert(displayToKey($event))"
+        />
+      </div>
+      <div class="ms__cost">
+
+      </div>
+    </custom-wrapper>
+  </section>
+</template>
+
+<script>
+import CustomWrapper from "@/components/elements/custom-wrapper";
+import CustomSelector from "@/components/elements/custom-selector";
+
+export default  {
+  name: "main-search",
+  components: {
+    CustomWrapper,
+    CustomSelector
+  },
+}
+</script>
